f05ceb5175f52099efc35ed03d9bbdd9bc04b381
praneethnanjuti <praneethnanjuti@gmail.com>
2026-07-01 17:21:03 +0530

    Use Render backend URL

--- autocare/src/services/api.js
+++ autocare/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://autocare-pro-backend-a2mg.onrender.com/api",
+  
 });
 
 API.interceptors.request.use((config) => {
